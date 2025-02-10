28e6a8b0b2e67b56274e394fca5cab17864af157
mahibhingradiya <126381179+mahibhingradiya@users.noreply.github.com>
2025-02-10 13:46:05 +0530

    implemnt in  chatpage and chatlayout search status

--- lib/Pages/Profile/Profile.dart
+++ lib/Pages/Profile/Profile.dart
@@ -1,18 +1,24 @@
 import 'package:chatapp/Pages/helpers/MainNavigation.dart';
 import 'package:chatapp/models/CustomClass.dart';
+import 'package:chatapp/models/CustomClass.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../services/auth_services.dart';
+import '../Authentication/FirstPage.dart';
 
 class Profile extends StatefulWidget {
   final CustomClass currentUser;
-  const Profile({super.key,required this.currentUser});
+
+  const Profile({super.key, required this.currentUser});
 
   @override
   State<Profile> createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
-
-  int _selectedIndex=3;
+  int _selectedIndex = 3;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -40,11 +46,208 @@ class _ProfileState extends State<Profile> {
         break;
     }
   }
+
+  String firstName = "";
+  String email = "";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  void fetchUserData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(user.uid)
+          .get();
+
+      if (userDoc.exists) {
+        setState(() {
+          firstName = userDoc["firstName"] ?? "";
+          email = userDoc["email"] ?? "";
+
+          // Capitalize first letter of firstName
+          if (firstName.isNotEmpty) {
+            firstName = firstName[0].toUpperCase() + firstName.substring(1);
+          }
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    final height = MediaQuery.sizeOf(context).height;
+
     return Scaffold(
-      body: Center(child: Text('data',style: TextStyle(fontSize: 30),),),
-    bottomNavigationBar: MainNavigationPage(currentIndex: _selectedIndex, onTap: _onItemTapped),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "Profile",
+            style: TextStyle(
+                fontFamily: 'poppins',
+
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.black54,
+                child: Text(
+                  firstName.isNotEmpty ? firstName[0].toUpperCase() : "",
+                  style: TextStyle(
+                    fontSize: width * 0.08,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: width * 0.02),
+              height: height * 0.1,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8, top: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.perm_identity,
+                      color: Colors.black45,
+                      size: width * 0.09,
+                    ),
+                    SizedBox(
+                      width: width * 0.03,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Name",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: width * 0.035,
+                              fontFamily: 'poppins',
+                              fontWeight: FontWeight.w700),
+                        ),
+                        SizedBox(
+                          height: height * 0.003,
+                        ),
+                        Text(
+                          firstName.isNotEmpty ? firstName : "",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Raleways',
+                              fontSize: width * 0.035),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: width * 0.02),
+              height: height * 0.08,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8, top: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.email_outlined,
+                      color: Colors.black45,
+                      size: width * 0.09,
+                    ),
+                    SizedBox(
+                      width: width * 0.05,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Email",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: width * 0.035,
+                              fontFamily: 'poppins',
+                              fontWeight: FontWeight.w700),
+                        ),
+                        SizedBox(
+                          height: height * 0.005,
+                        ),
+                        Text(
+                          email.isNotEmpty ? email : "",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Raleways',
+                              fontSize: width * 0.035),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: (){
+                logOutUser().then((_){
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => Firstpage()), // Navigate to login screen
+                        (Route<dynamic> route) => false, // Remove all previous routes from stack
+                  );
+                });
+
+              },
+
+              leading: Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+              title: Text(
+                "Log out",
+                style: TextStyle(fontFamily: 'Raleway', color: Colors.red),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: MainNavigationPage(
+          currentIndex: _selectedIndex, onTap: _onItemTapped),
     );
   }
 }
