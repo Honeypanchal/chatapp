696d8e1312546d2c24e1dd6ac18f675f0227815c
mahibhingradiya <126381179+mahibhingradiya@users.noreply.github.com>
2025-02-10 13:36:16 +0530

    implemnt in  chatpage and chatlayout search status

--- lib/Pages/Chatlayout/ChatPage.dart
+++ lib/Pages/Chatlayout/ChatPage.dart
@@ -5,6 +5,254 @@ import 'package:chatapp/pages/helpers/MainNavigation.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+class ChatPage extends StatefulWidget {
+  final CustomClass currentUser;
+
+  ChatPage({required this.currentUser});
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _database = FirebaseFirestore.instance.collection('Users');
+  dynamic chatsDB = FirebaseFirestore.instance.collection("chats");
+  List<Map<String, dynamic>> _selectedUsers = [];
+  List<Map<String, dynamic>> _filteredUsers = [];
+  bool _isLoading = false;
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    loadSelectedUsers();
+    _searchController.addListener(_filterUsers);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_filterUsers);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/chatPage',
+            arguments: {'currentUser': widget.currentUser});
+        break;
+      case 1:
+        Navigator.pushNamed(
+          context,
+          '/groupDisplay',
+          arguments: {'currentUser': widget.currentUser},
+        );
+        break;
+      case 2:
+        Navigator.pushNamed(context, '/statusPage');
+        break;
+      case 3:
+        Navigator.pushNamed(context, '/profile',
+            arguments: {'currentUser': widget.currentUser});
+        break;
+    }
+  }
+
+  /*Future<void> loadSelectedUsers() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    DocumentSnapshot userDoc = await _database.doc(widget.currentUser.uid).get();
+    if (userDoc.exists && userDoc.data() != null) {
+      List<dynamic> savedUsers = userDoc.get("selectedUsers") ?? [];
+      setState(() {
+        _selectedUsers = List<Map<String, dynamic>>.from(savedUsers);
+        _filteredUsers = _selectedUsers;
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }*/
+  Future<void> loadSelectedUsers() async {
+    DocumentSnapshot userDoc = await _database.doc(widget.currentUser.uid).get();
+    if (userDoc.exists && userDoc.data() != null) {
+      List<dynamic> savedUsers = userDoc.get("selectedUsers") ?? [];
+      setState(() {
+        _selectedUsers = List<Map<String, dynamic>>.from(savedUsers);
+        _filteredUsers = _selectedUsers;
+      });
+    }
+  }
+
+
+  void _filterUsers() {
+    String query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredUsers = _selectedUsers
+          .where((user) => user['firstName'].toLowerCase().contains(query))
+          .toList();
+    });
+  }
+
+  /*void navigateToChat(Map<String, dynamic> user) {
+    String docId = widget.currentUser.uid.compareTo(user['uid']) < 0
+        ? "${widget.currentUser.uid}_${user['uid']}"
+        : "${user['uid']}_${widget.currentUser.uid}";
+
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => ChatLayout(
+        currentUser: widget.currentUser,
+        user: user,
+        databaseRef: chatsDB.doc(docId),
+      ),
+    ));
+  }*/
+  void navigateToChat(Map<String, dynamic> user) async {
+    String docId = widget.currentUser.uid.compareTo(user['uid']) < 0
+        ? "${widget.currentUser.uid}_${user['uid']}"
+        : "${user['uid']}_${widget.currentUser.uid}";
+
+    // Ensure both users have this chat in their chat lists
+    await _database.doc(widget.currentUser.uid).update({
+      "selectedUsers": FieldValue.arrayUnion([user])
+    });
+
+    await _database.doc(user['uid']).update({
+      "selectedUsers": FieldValue.arrayUnion([
+        {
+          "uid": widget.currentUser.uid,
+          "firstName": widget.currentUser.firstName
+        }
+      ])
+    });
+
+    // Navigate to chat
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => ChatLayout(
+        currentUser: widget.currentUser,
+        user: user,
+        databaseRef: chatsDB.doc(docId),
+      ),
+    ));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+        title: Text(
+          'Chats',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        /*actions: [
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],*/
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: "Search users...",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: _isLoading
+                ? Center(child: CircularProgressIndicator(color: Colors.black))
+                : _filteredUsers.isEmpty
+                ? Center(child: Text("No chats available. Click + to start a chat."))
+                : ListView.builder(
+              itemCount: _filteredUsers.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  onTap: () => navigateToChat(_filteredUsers[index]),
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Text(
+                      "${_filteredUsers[index]['firstName'][0].toUpperCase()}",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    "${_filteredUsers[index]['firstName']} ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Map<String, dynamic>? selectedUser = await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => UserListPage(currentUser: widget.currentUser),
+            ),
+          );
+
+          if (selectedUser != null) {
+            setState(() {
+              _selectedUsers.add(selectedUser);
+              _filteredUsers = _selectedUsers;
+            });
+          }
+        },
+        backgroundColor: Colors.green.shade700,
+        child: Icon(Icons.add, color: Colors.black, size: 40),
+      ),
+      bottomNavigationBar:
+      MainNavigationPage(currentIndex: _selectedIndex, onTap: _onItemTapped),
+    );
+  }
+}
+
+
+/*
+import 'package:chatapp/models/CustomClass.dart';
+import 'package:chatapp/Pages/Chatlayout/chat_layout.dart';
+import 'package:chatapp/Pages/Chatlayout/user_list.dart';
+import 'package:chatapp/pages/helpers/MainNavigation.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class ChatPage extends StatefulWidget {
 
@@ -226,3 +474,4 @@ class _ChatPageState extends State<ChatPage> {
 
 
 
+*/
--- lib/Pages/Chatlayout/user_list.dart
+++ lib/Pages/Chatlayout/user_list.dart
@@ -14,6 +14,140 @@ class UserListPage extends StatefulWidget {
 class _UserListPageState extends State<UserListPage> {
   final _database = FirebaseFirestore.instance.collection('Users');
   dynamic chatsDB = FirebaseFirestore.instance.collection("chats");
+  List<Map<String, dynamic>> _allUsers = [];
+  List<Map<String, dynamic>> _filteredUsers = [];
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    loadUsers();
+    _searchController.addListener(_filterUsers);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_filterUsers);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  Future<void> loadUsers() async {
+    QuerySnapshot querySnapshot = await _database.get();
+    List<Map<String, dynamic>> users = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+
+    setState(() {
+      _allUsers = users.where((user) => user['uid'] != widget.currentUser.uid).toList();
+      _filteredUsers = _allUsers;
+    });
+  }
+
+  void _filterUsers() {
+    String query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredUsers = _allUsers
+          .where((user) => user['firstName'].toLowerCase().contains(query))
+          .toList();
+    });
+  }
+
+  void navigateToChat(Map<String, dynamic> user) {
+    String docId = widget.currentUser.uid.compareTo(user['uid']) < 0
+        ? "${widget.currentUser.uid}_${user['uid']}"
+        : "${user['uid']}_${widget.currentUser.uid}";
+
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => ChatLayout(
+        currentUser: widget.currentUser,
+        user: user,
+        databaseRef: chatsDB.doc(docId),
+      ),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.black,
+        title: Text('Select a User', style: TextStyle(color: Colors.white)),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: "Search users...",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: _filteredUsers.isEmpty
+                ? Center(child: Text("No users available."))
+                : ListView.builder(
+              itemCount: _filteredUsers.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> user = _filteredUsers[index];
+                return ListTile(
+                  onTap: () {
+                    navigateToChat(user);
+                  },
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Text(
+                      "${user['firstName'][0].toUpperCase()}",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  title: Text(
+                    "${user['firstName']} ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+/*
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chatapp/models/CustomClass.dart';
+import 'package:chatapp/Pages/Chatlayout/chat_layout.dart';
+
+class UserListPage extends StatefulWidget {
+  final CustomClass currentUser;
+  UserListPage({required this.currentUser});
+
+  @override
+  _UserListPageState createState() => _UserListPageState();
+}
+
+class _UserListPageState extends State<UserListPage> {
+  final _database = FirebaseFirestore.instance.collection('Users');
+  dynamic chatsDB = FirebaseFirestore.instance.collection("chats");
   List<Map<String, dynamic>> _selectedUsers = [];
 
   @override
@@ -130,3 +264,4 @@ class _UserListPageState extends State<UserListPage> {
     );
   }
 }
+*/
